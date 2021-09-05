34310c790d3c432d34e4b3e56c875aa4834bc801
Anas AbuGhalieh <civil.Anas1000@gmail.com>
2021-09-05 21:15:10 +0300

    initial code

---
+++ post/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=64)
+    rating = models.IntegerField(max_length=64)
+    body = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
