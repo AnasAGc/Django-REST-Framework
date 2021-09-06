6ac16c7b44ebddc39d465286187068310d42f551
Anas AbuGhalieh <civil.Anas1000@gmail.com>
2021-09-06 13:39:53 +0300

    adding the test

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@3&_wil&zq6v726l3h+6gn#3_#i#p2$!#tlh5p&62n6m#xcr9-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0','localhost','127.0.0.1']
 
 
 # Application definition
